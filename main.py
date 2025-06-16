01fd5f6a1511aed1872e92186a2bfbaf59344469
8ab7bbdf01a24e988c50c4cfe9557814 <jonas.lemmon@det.nsw.edu.au>
2025-06-17 08:43:35 +1000

    Initial file upload
    Separate class files to allow programmers to work more independently

---
+++ main.py
@@ -0,0 +1,11 @@
+#This is the main program code
+
+#Import classes from external files - the first one is done - you add the rest
+from headTeacher import *
+
+
+#Define any subprograms you might need here
+
+
+#The mainline program code 
+#Test the classes by adding one object of each and interacting with their methods to ensure they behave correctly
