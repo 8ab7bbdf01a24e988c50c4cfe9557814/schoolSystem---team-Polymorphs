b68e63989c87fe1c6a140322b8631b81823bff3b
VarifiedAccount <finnley.hurst@education.nsw.gov.au>
2025-06-19 14:20:06 +1000

    Update primaryStudent.py

--- primaryStudent.py
+++ primaryStudent.py
@@ -1,3 +1,10 @@
 #Create a PrimaryStudent class in this file
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
+class PrimaryStudent (student):
+    def _init_(self, name, schoolYear):
+        super()._init_(name, schoolYear)
+        print("Created (self.name)in a schoolYear). PrimaryStudent")
+    def greet(self, person=None):
+        if person=None:
+            print("no")
